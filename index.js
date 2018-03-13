887789c68be3834758d9a924787f08860ebef830
Valerie Falstad <valeriefalstad@valeries-mbp.attlocal.net>
2018-03-13 10:15:23 -0500

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 8080,
   mongoose = require('mongoose'),
   Question = require('./api/models/resumetestModel'), //created model loading here
   bodyParser = require('body-parser');
